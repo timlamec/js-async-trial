7db2b51c91ccfe9a88fe5945ef4330d014aa9f1b
timlamec <55591841+timlamec@users.noreply.github.com>
2022-05-09 14:04:52 +0300

    more

--- async example2.js
+++ async example2.js
@@ -19,13 +19,26 @@ function time(ms){
 
 async function kitchen () {
     try{
-        
+        await time(3000)
+        console.log(`${stocks.Cereal[1]}`)
+
+        await time(0000)
+        console.log("start production")
+
+        await time(1000)
+        console.log("cut vegetables")
+
+        await time(1000)
+        console.log("cut fruits")
+
+        await time(2000)
+        console.log(`${stocks.Fruits[1]} and ${stocks.Fruits[2]} was added to the cart`)
     }
     catch(error) {
         console.log("customer left", error)
     }
     finally{
-        console.log("shop is closed");
+        console.log("day ended shop is closed");
     }
     
 }
